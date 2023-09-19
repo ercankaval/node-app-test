4d90660bbd78c662968237618c7ac8e68c8c1da2
Ercan Kaval <ercankaval@Ercans-Laptop.local>
2023-09-19 16:32:24 +0300

    changed files

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log('ercan kaval');
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res) => {
+    console.log('console message')
+    res.send("first app")
+})
+
+app.listen(3000)
